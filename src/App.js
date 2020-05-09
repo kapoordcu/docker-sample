e7b865076a0c0701fd9913ff115710e4392f0b1f
kapoordcu <gaurav.kapoor23@outlook.com>
2020-05-09 04:31:26 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js ----</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -19,6 +19,9 @@ function App() {
           Learn React
         </a>
       </header>
+      <p>
+        Hi there
+      </p>
     </div>
   );
 }
